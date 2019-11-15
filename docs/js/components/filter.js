0bece928c926c797413c79f9abd299cba9dc3755
Aaron <troublor@live.com>
2019-11-15 18:34:07 +0800

    fix bug when last character in search textbox is deleted, the search result does not refresh

--- docs/js/components/filter.js
+++ docs/js/components/filter.js
@@ -34,7 +34,7 @@ Vue.component('filter-component', {
     },
     watch: {
         searchPayload: function (val) {
-            if (val.length > 0) {
+            if (val.length >= 0) {
                 let tmp = val.trim().split(' ');
                 let kws = [];
                 for (let i = 0; i < tmp.length; i++) {
